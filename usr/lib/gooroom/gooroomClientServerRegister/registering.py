f41dc33361548ddca1a267675eca30478af00322
taisun_park <taisun_park@tmax.co.kr>
2023-03-10 13:46:00 +0900

    Import Debian version 1.3.0+grm3u1tos2
    gooroom-client-server-register (1.3.0+grm3u1tos2) tmax-unstable; urgency=medium
    .
      [ taisun_park ]
      * [e4fb6f3] debian/control maintainer, homepage 수정
    .
    gooroom-client-server-register (1.3.0+grm3u1tos1) tmax-unstable; urgency=medium
    .
      [ taisun_park ]
      * [c0e51b7] (MOIS) MASTER 조인 시 SLAVE CONF 처리 변경
      * [830b022] [chore] gdm에 대한 처리 추가 - user_id 가져오는 부분 gdm에 대한 처리 추가
    .
      [ jeonghan_bae ]
      * [8990075] symlink parameter edit
      * [bd3d87c] gcsr_slave and symlink logic added
    .
    gooroom-client-server-register (1.3.0+grm3u1) unstable; urgency=medium
    .
      [ seolhee.sun ]
      * [c8b1d546] gooroom-3.0 UI update
      * [23d0e648] StaticCodeAnalysis warning modify
    .
    gooroom-client-server-register (1.0.8+grm2u1) unstable; urgency=medium
    .
      [ junsungc ]
      * [11f969ab] Change sequence (CSR - getHost) Add kcmvp module & Read kcmvp_on_off from headers
      * [0871d376] 'ip_type' Bug fix. The bug that occurs when gcsr receive a  location on root CA path
      * [39d2a96c] Remove libMagicCrypto *.so files
    .
      [ Gooroom Autobuilder ]
    .
    gooroom-client-server-register (1.0.7+grm2u1) unstable; urgency=medium
    .
      [ junsungc ]
      * [f7df8df9] Save gkm-server public key on /etc/ssl/private
    .
      [ Gooroom Autobuilder ]
    .
    gooroom-client-server-register (1.0.6+grm2u1) unstable; urgency=medium
    .
      [ junsungc ]
      * [6f2e9b47] Changed the organizational-unit(on) value not necessary in cli mode.
    .
      [ Gooroom Autobuilder ]
    .
    gooroom-client-server-register (1.0.5+grm2u1) unstable; urgency=medium
    .
      [ junsungc ]
      * [eecd214c] Support IPv6 for server
      * [65dcaba9] bug fixed 2087
      * [8541d75b] bug fixed 2429
    .
      [ Gooroom Autobuilder ]
    .
    gooroom-client-server-register (1.0.4+grm2u1) unstable; urgency=medium
    .
      [ junsungc ]
      * [ac950e85] Support IPv6
    .
      [ Gooroom Autobuilder ]
    .
    gooroom-client-server-register (1.0.3+grm2u1) unstable; urgency=medium
    .
      [ junsungc ]
      * [9755a537] fixed 0001826
      * [43b588f1] fixed 0001830
      * [fb7181c3] Change common-name(cn) value. [mac -> mac+hash(hardware SN)]
      * [48449958] Delete the password_system_type UI. The password_system_type in the gcsr.conf file contains [sha256]
    .
      [ Gooroom Autobuilder ]
    .
    gooroom-client-server-register (1.0.2+grm2u1) unstable; urgency=medium
    .
      * Initial Release

---
+++ usr/lib/gooroom/gooroomClientServerRegister/registering.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+import gettext
+import os
+import getpass
+import pprint
+import threading
+import time
+import copy
+import hashlib
+import codecs
+
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
+from gi.repository import Gdk, Gtk
+
+import certification
+import subprocess
+
+from pwd import getpwnam
+
+gettext.bindtextdomain('gooroom-client-server-register', '/usr/share/locale')
+gettext.textdomain('gooroom-client-server-register')
+_=gettext.gettext
+
+class Registering():
+    "Registering parent class"
+    def __init__(self):
+        self.WORK_DIR = '/usr/lib/gooroom/gooroomClientServerRegister'
+
+    def result_format(self, result):
+        "Return result log pretty"
+        # TODO: formatting more pretty
+        result_text = ''
+        for text in result:
+            result_text += pprint.pformat(text) + '\n'
+
+        return result_text
+
+    def make_hash_cn(self):
+        cmd = subprocess.run(['/usr/sbin/dmidecode', '-s', 'system-serial-number'], stdout=subprocess.PIPE, universal_newlines=True)
+        result = cmd.stdout.rstrip() + '/'
+        cmd = subprocess.run(['/usr/sbin/dmidecode', '-s', 'system-uuid'], stdout=subprocess.PIPE, universal_newlines=True)
+        result += cmd.stdout.rstrip() + '/'
+        cmd = subprocess.run(['/usr/sbin/dmidecode', '-s', 'baseboard-serial-number'], stdout=subprocess.PIPE, universal_newlines=True)
+        result += cmd.stdout.rstrip()
+        hash_result = hashlib.md5(result.encode()).hexdigest()
+        base64_result = codecs.encode(codecs.decode(hash_result, 'hex'), 'base64').decode().rstrip()
+        return base64_result
+
+    def make_mac(self):
+        """
+        make cn with sn + mac
+        """
+
+        ENP_PATH = '/sys/class/net/enp0s3/address'
+        if os.path.exists(ENP_PATH):
+            with open(ENP_PATH) as f:
+                cn = f.read().strip('\n').replace(':', '')
+                print('enp0s3={}'.format(cn))
+                return cn
+        else:
+            import glob
+            ifaces = [i for i in glob.glob('/sys/class/net/*')]
+            ifaces.sort()
+            for iface in ifaces:
+                if iface == '/sys/class/net/lo':
+                    continue
+                with open(iface+'/address') as f2:
+                    cn = f2.read().strip('\n').replace(':', '')
+                    print('iface={}'.format(cn))
+                    return cn
+            return 'CN-NOT-FOUND-ERROR'
+
+    def make_cn(self):
+
+        CN_PATH = '/etc/gooroom/gooroom-client-server-register/gcsr_master.conf'
+        if os.path.exists(CN_PATH):
+            try:
+                import configparser
+                parser = configparser.RawConfigParser()
+                parser.optionxform = str
+                parser.read(CN_PATH)
+                cn = parser.get('certificate', 'client_name').strip().strip('\n')
+                print('gcsr_master.conf={}'.format(cn))
+                return cn
+            except:
+                pass
+
+        cn = self.make_mac()
+        return cn + self.make_hash_cn()
+
+    def make_ipname(self):
+        """
+        make name with IP
+        """
+        return os.popen('hostname --all-ip-addresses').read().split(' ')[0]
+
+    def make_ipv6name(self):
+        """
+        make name with IPv6
+        """
+        return os.popen('/sbin/ip -6 addr | grep inet6 | awk -F \'[ \t]+|/\' \'{print $3}\' | grep -v ^::1').read().split('\n')[0]
+
+
---
+++ usr/sbin/gpms-auto-registration.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#
+# Automatic GPMS registration script 
+#  written by Gooroom Project Team <shjung@gooroom.kr>
+#
+
+if [ $ENABLE_AUTO_REGI != 'Yes' ] 
+then
+        exit 0
+fi
+
+if [ -z $CLIENT_NAME ]
+then
+	CMD="gooroom-client-server-register noninteractive-regkey -d $GKM_SERVER -u "AUTO_REGED" -k $REG_KEY -r 2"
+else
+	CMD="gooroom-client-server-register noninteractive-regkey -d $GKM_SERVER -m $CLIENT_NAME -u "AUTO_REGED" -k $REG_KEY -r 2"
+fi
+
+if [ -z $GKM_SERVER ] || [ -z $REG_KEY ]
+then
+        echo Configuration is not properly set
+	echo $GKM_SERVER >> /tmp/gpms_autoregi_debug.txt
+	echo $REG_KEY >> /tmp/gpms_autoregi_debug.txt
+	echo $CLIENT_NAME >> /tmp/gpms_autoregi_debug.txt
+        exit 1
+fi
+
+systemctl enable gooroom-agent
+systemctl start gooroom-agent
+sleep 1
+if ( dbus-send --system --print-reply --type=method_call --dest='kr.gooroom.agent' '/kr/gooroom/agent' kr.gooroom.agent.do_task string:'{"module" : {"module_name" : "SERVER","task" : {"task_name" : "grm_heartbit","in" : {}}}}' | grep "\"status\"\:\ \"200\"" )
+#if ( ! true  ) 
+then
+	rm -f /etc/gooroom/gooroom-client-server-register/gcsr_auto.conf
+	echo gpms-server is already connected.
+else
+	systemctl stop gooroom-agent
+	DO="yes"
+	while [ $DO == "yes"  ]
+	do
+		
+		if ( $CMD )
+		then
+			DO="no"	
+			rm -f /etc/gooroom/gooroom-client-server-register/gcsr_auto.conf
+		fi
+	    sleep 2
+	done
+	systemctl enable gooroom-agent
+	systemctl start gooroom-agent
+fi
